4c3d27f20f6205bc2516fdb290e7034341e13331
Gigi Minova <minova.gigi@gmail.com>
2019-11-27 09:39:07 +0000

    Updates

---
+++ precache-manifest.5cb1d1bab176a3a01581450c2e84257d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139de5a9a5364abfc429eff4a890d37b",
+    "url": "/to-do-redux/index.html"
+  },
+  {
+    "revision": "494b2cadcfeb74e126c0",
+    "url": "/to-do-redux/static/js/2.f8e2c626.chunk.js"
+  },
+  {
+    "revision": "f004440fcf389cf6d3a4",
+    "url": "/to-do-redux/static/js/main.75bba7ff.chunk.js"
+  },
+  {
+    "revision": "ba949427e57cd0f40264",
+    "url": "/to-do-redux/static/js/runtime-main.a583e5db.js"
+  }
+]);
